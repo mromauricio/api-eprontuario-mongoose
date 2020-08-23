02e52af290e4f2fdd95593d927369f8bec4f3543
Mauricio Oliveira <mauriciooliveira@Mac.local>
2020-08-23 16:17:15 -0300

    update

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());      
 
-var infopacienteRouter = require('./routes/infopaciente.js');
-app.use('/infopaciente', infopacienteRouter);
+var infopaciente = require('./controllers/infopacienteController.js');
+app.use('/infopaciente', infopaciente);
 
 const port = 3000;
 app.listen(port, () => {
